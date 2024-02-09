c8ec8282362d2a8d0d4cc63256a6cddc04abb816
hassan <hchoudhury20@hotmail.com>
2024-02-09 18:02:09 +0000

    initial commit

---
+++ test/Controllers/testController.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.SignalR;
+using test.models;
+
+namespace test.Controller{
+    [ApiController]
+    [Route("api/test")]
+
+    public class testController: ControllerBase
+{
+
+
+
+        [HttpGet(Name = "GetCarDetails")]
+        public IEnumerable<Car> Get()
+        {
+            return new List<Car>{
+                new Car()
+                {
+                    name="test",
+                    weight = 123,
+                    registration = "t3st"       
+                },
+                   new Car()
+                {
+                    name="test1",
+                    weight = 1234,
+                    registration = "t3st1"
+                }
+            };
+
+        }
+    
+}
+}
---
+++ test/models/Car.cs
@@ -0,0 +1,12 @@
+﻿namespace test.models;
+
+public class Car()
+{
+
+    public string? name{get; set;}
+
+    public string? registration{get; set;}
+
+    public decimal weight {get; set;}
+
+}
